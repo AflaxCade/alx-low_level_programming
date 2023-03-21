8175a1b8c3e2cb222288ff6494d1c3a155c51d28
AlaxCade <aflah.cade@gmail.com>
2023-03-21 08:57:39 -0700

    added 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	long int a, b, c;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (i = 0; i < 48; i++)
+	{
+		c = a + b;
+		printf(", %ld", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
