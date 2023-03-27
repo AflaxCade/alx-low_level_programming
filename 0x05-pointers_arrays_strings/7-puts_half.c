70496547c3737d7e0b9320b7d35283fec7964870
AlaxCade <aflah.cade@gmail.com>
2023-03-27 06:26:26 -0700

    added half of the files

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to a char
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		i = len / 2;
+	}
+	else
+	{
+		i = (len + 1) / 2;
+	}
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
