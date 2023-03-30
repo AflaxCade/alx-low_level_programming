50b1a6f425b8bb0308c53d660ae74108deff619f
AlaxCade <aflah.cade@gmail.com>
2023-03-30 02:06:13 -0700

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * infinite_add - adds two numbers
@@ -9,40 +9,34 @@ include "main.h"
  *
  * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l1 = strlen(n1);
+	int l2 = strlen(n2);
+	int sum = 0;
+	int carry = 0;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
+	if (l1 > size_r || l2 > size_r)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+
+	for (i = l1 - 1, j = l2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k++)
 	{
-		n = m;
+		sum = carry;
 		if (i >= 0)
-			n += n1[i] - '0';
+			sum += n1[i] - '0';
 		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+			sum += n2[j] - '0';
+		carry = sum / 10;
+		sum %= 10;
+		r[k] = sum + '0';
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (k > size_r)
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
+	r[k] = '\0';
+	for (i = 0, j = k - 1; i < j; i++, j--)
+		swap_char(&r[i], &r[j]);
 	return (r);
 }
