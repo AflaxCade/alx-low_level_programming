f86010443e5a5c533467a097ac632fb79264797e
AlaxCade <aflah.cade@gmail.com>
2023-03-30 02:13:22 -0700

    added 103-infinite_add.c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,34 +9,40 @@
  *
  * Return: pointer to the result
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int l1 = strlen(n1);
-    int l2 = strlen(n2);
-    int sum = 0;
-    int carry = 0;
+	int i, j, k, l, m, n;
 
-    if (l1 > size_r || l2 > size_r)
-        return (0);
-
-    for (i = l1 - 1, j = l2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k++)
-    {
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-        carry = sum / 10;
-        sum %= 10;
-        r[k] = sum + '0';
-    }
-    if (k > size_r)
-        return (0);
-    r[k] = '\0';
-    for (i = 0, j = k - 1; i < j; i++, j--)
-        swap_char(&r[i], &r[j]);
-    return (r);
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
