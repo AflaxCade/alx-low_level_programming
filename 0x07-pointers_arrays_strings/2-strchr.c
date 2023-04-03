872b58e6f3c946ca5cfbf0fff021598cc7e40caa
AlaxCade <aflah.cade@gmail.com>
2023-04-03 02:23:31 -0700

    added 2-strchr.c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
+	do {
 		if (*s == c)
+		{
 			return (s);
-		*s++;
-	}
-	if (*s == c)
-		return (s);
+		}
+	} while (*s++);
 	return (NULL);
 }
