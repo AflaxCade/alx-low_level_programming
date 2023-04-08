bea3e42d5600a7834ffe8760f982be3c2810ee34
AlaxCade <aflah.cade@gmail.com>
2023-04-08 03:51:16 -0700

    added 2 files

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int m1, m2;
+	int x, y;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	m1 = atoi(argv[1]);
-	m2 = atoi(argv[2]);
-	printf("%d\n", m1 * m2);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 	return (0);
 }
