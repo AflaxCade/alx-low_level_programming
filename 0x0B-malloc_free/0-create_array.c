c904b874592996bd8b17fce4304e9f8ae2a104eb
AlaxCade <aflah.cade@gmail.com>
2023-04-11 02:26:40 -0700

    added 0-create_array.c file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,18 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	char *array = malloc(sizeof(char) * size);
+	char *array;
+	
+	unsigned int i;
+
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
