ea6ac3718fbed73bbc88fe1fce6548c752b0b0ef
AlaxCade <aflah.cade@gmail.com>
2023-04-17 06:39:13 -0700

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include "dog.h"
 
-typedef struct dog dog_t;
-
 /**
  * new_dog - creates a new dog_t struct and returns a pointer to it
  * @name: the name of the dog
@@ -15,32 +13,33 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	
+
 	new_dog = malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
 	new_dog->name = malloc(strlen(name) + 1);
+
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
 	strcpy(new_dog->name, name);
 
 	new_dog->age = age;
 
 	new_dog->owner = malloc(strlen(owner) + 1);
+
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
 	strcpy(new_dog->owner, owner);
 
 	return (new_dog);
