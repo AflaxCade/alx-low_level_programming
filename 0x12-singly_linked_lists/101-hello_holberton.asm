6f6c3f62b5548ed894d133a3b343ce64d9a634bf
AlaxCade <aflah.cade@gmail.com>
2023-04-27 04:11:20 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message before the main function is executed
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+	return (0);
+}
