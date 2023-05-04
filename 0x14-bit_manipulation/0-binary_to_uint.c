4a1132366077e2bd3846dd4d023e32ffc40395c0
AlaxCade <aflah.cade@gmail.com>
2023-05-04 09:21:14 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,24 +5,21 @@
  * @b: pointing to a string of 0 and 1 chars
  * Return:converted number, or 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int result;
-
 	result = 0;
+
 	if (!b)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		result = 2 * result + (b[i] - 0);
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		result <<= 1;
-		if (b[i] == '1')
-			result += 1;
-	}
+
 	return (result);
 }
